d6b7b6b3d44a724610862935fdff436297e5223b
Logan Brock <36110596+lgbrock@users.noreply.github.com>
2021-01-17 13:19:11 -0600

    final update

--- js/main.js
+++ js/main.js
@@ -1,11 +1,28 @@
 // start with strings, numbers and booleans
+/*
+let age = 100;
+let age2 = age;
+console.log(age, age2);
+age = 200;
+console.log(age, age2);
+
+let name = 'Logan';
+let name2 = name;
+console.log(name, name2);
+name = 'Brock';
+console.log(name, name2);
+*/
 
 // Let's say we have an array
 const players = ['Wes', 'Sarah', 'Ryan', 'Poppy'];
 
 // and we want to make a copy of it.
+const team = players;
+
+console.log([players, team]);
 
 // You might think we can just do something like this:
+// team[3] = 'Lux';
 
 // however what happens when we update that array?
 
@@ -16,12 +33,19 @@ const players = ['Wes', 'Sarah', 'Ryan', 'Poppy'];
 // Why? It's because that is an array reference, not an array copy. They both point to the same array!
 
 // So, how do we fix this? We take a copy instead!
+const team2 = players.slice();
 
 // one way
 
 // or create a new array and concat the old one in
+const team3 = [].concat(players);
 
 // or use the new ES6 Spread
+const team4 = [...players];
+team[3] = 'hello'
+console.log(team4);
+
+const team5 = Array.from(players)
 
 // now when we update it, the original one isn't changed
 
@@ -33,10 +57,29 @@ const person = {
     age: 80
 };
 
-    // and think we make a copy:
+// and think we make a copy:
+// const captain = person;
+// captain.number = 99;
 
-    // how do we take a copy instead?
+// how do we take a copy instead?
+const cap2 = Object.assign({}, person, { number: 99 });
+console.log(cap2)
 
-    // We will hopefully soon see the object ...spread
+// We will hopefully soon see the object ...spread
+// const cap3 = { ...person }
 
-    // Things to note - this is only 1 level deep - both for Arrays and Objects. lodash has a cloneDeep method, but you should think twice before using it.
+// Things to note - this is only 1 level deep - both for Arrays and Objects. lodash has a cloneDeep method, but you should think twice before using it.
+const logan = {
+    name: 'Logan',
+    age: 29,
+    social: {
+        twitter: '@lifeisagarden23'
+    }
+};
+
+console.clear();
+console.log(logan);
+
+const dev = Object.assign({}, logan);
+
+const dev2 = JSON.parse(JSON.stringify(logan));
